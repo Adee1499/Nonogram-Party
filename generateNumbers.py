1ae14e5016b10a1b58de70870f277467578fa941
Adrian Szarapow <63786007+Adee1499@users.noreply.github.com>
2021-01-24 00:43:22 +0000

    generates numbers for grid from JSON
    Only first line correct, rest is weird. Need to check what's wrong

---
+++ generateNumbers.py
@@ -0,0 +1,31 @@
+import json;
+import pprint;
+
+json_grid = open('1.json')
+
+grid = json.load(json_grid)
+
+#pprint.pprint(grid)
+
+#print(grid)
+
+#print(grid[0])
+
+height = len(grid)
+width = len(grid[0])
+
+print("This nonogram is " + str(height) + "X" + str(width))
+
+a = []
+
+for row in grid:
+    temp = 0
+    for element in row:
+        if element == 1:
+            temp += 1
+        elif element == 0:
+            if temp != 0:
+                a.append(temp)
+            temp = 0
+
+print(a)
