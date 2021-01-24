ffba800767ea7f1fabde7340206cfd85340113e2
Adrian Szarapow <63786007+Adee1499@users.noreply.github.com>
2021-01-24 00:18:12 +0000

    GUI now converts successfully to JSON
    Still need to make the text file assign an appropriate name to the output file, plus after clicking convert to JSON, it should output a grid with numbers (no code for this yet)

--- gui_test.py
+++ gui_test.py
@@ -1,18 +1,49 @@
+import pprint
+import os
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
 def import_file():
     global photo
+    global image
     filename = filedialog.askopenfilename(title = 'Select nonogram image',filetypes = (('file_type','*.png, *.PNG'),('all files','*.*')))
-    im = Image.open(filename)
-    size = im.size[0]
+    image = Image.open(filename)
+    size = image.size[0]
     photo = PhotoImage(file=filename)
     multiplier = 15
     photo = photo.zoom(multiplier, multiplier)
     canvas = Canvas(width=size*multiplier, height=size*multiplier)
     canvas.create_image(0, 0, image=photo, anchor=NW)
     canvas.pack()
+    return filename
+
+def convert_to_json():
+    pix = image.load()
+    sizeX = image.size[0]
+    sizeY = image.size[1]
+
+    m = []
+
+    for y in range(sizeX):
+        for x in range(sizeY):
+            if 0 in pix[x, y]:
+                m.append(1)
+            else:
+                m.append(0)
+
+    def chunks(lst, n):
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+    global preJSON
+    preJSON = chunks(m, sizeY)
+
+def save_to_file():
+    OUTPUT_DIR = filedialog.askdirectory()
+    f = open(os.path.join(OUTPUT_DIR, 'output.txt'), 'w')
+    pprint.pprint(list(preJSON), f)
+    f.close()
+
 
 window = Tk()
 window.title('Nonogram JSON Generator')
@@ -24,11 +55,12 @@ bottomFrame = Frame(window)
 bottomFrame.pack(side=BOTTOM)
 
 button1 = Button(topFrame, text="Select nonogram image", command=import_file)
-button2 = Button(topFrame, text="Convert to JSON")
-button3 = Button(bottomFrame, text="Save JSON")
+button2 = Button(topFrame, text="Convert to JSON", command=convert_to_json)
+button3 = Button(bottomFrame, text="Save JSON", command=save_to_file)
 
 button1.pack(side=LEFT)
 button2.pack(side=LEFT)
 button3.pack(side=BOTTOM)
 
 window.mainloop()
+
